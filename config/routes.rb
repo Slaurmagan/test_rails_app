bd6148f655b9a46ce2bd7c7ad3044eb428b4b264
Slaurmagan <s.hrakovich33@mail.ru>
2019-12-14 16:52:33 +0300

    Preparing for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'welcome/home' => 'welcome#home'
-  get 'welcome/about' => 'welcome#about'
-  
+  root 'welcome#home'
+  get '/about' => 'welcome#about'
+
 end
