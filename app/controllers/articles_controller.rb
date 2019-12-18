8f1e9500532d47e61e4dbbf351a175992c3d3b33
Slaurmagan <s.hrakovich33@mail.ru>
2019-12-18 23:48:58 +0300

    Added edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+
+
     def index
         
     end
@@ -19,6 +22,23 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def edit
+        @article = Article.find(params[:id])
+        
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+
+        end
+
+    end
+
     private
     def article_params
         params.require(:article).permit(:title,:description)
