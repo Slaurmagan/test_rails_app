fc37328c4db8603a5aa7905f4b13c3507b6c2fe8
Slaurmagan <s.hrakovich33@mail.ru>
2019-12-22 21:25:51 +0300

    Added some tests,categories model,listing and new actions

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+gem 'rails-controller-testing'
 
 gem 'bcrypt'
 # Use Redis adapter to run Action Cable in production
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  resources :users,expect: [:new]  
+  resources :users,except: [:new]  
+  resources :categories,except: [:destroy]
 
 end
