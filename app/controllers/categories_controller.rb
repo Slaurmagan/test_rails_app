cc3b2a66e099a3899d61466c227ebcea925fe0f9
Slaurmagan <s.hrakovich33@mail.ru>
2019-12-23 23:09:59 +0300

    Edited listing of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,13 @@
 class CategoriesController < ApplicationController
-
+    before_action :require_admin,only: [:new,:create]
     def index
+
         @categories = Category.paginate(page:params[:page],per_page:3)
     end
 
     def show
-
+        @category = Category.find(params[:id])
+        @category_articles = @category.articles.paginate(page:params[:page],per_page:5)
     end
 
     def new
@@ -28,5 +30,22 @@ class CategoriesController < ApplicationController
 
     def category_params
         params.require(:category).permit(:name)
+    end
+    def require_admin
+
+        if !logged_in? || (logged_in? && !current_user.admin?)
+            flash[:danger] = "Only admins can add category"
+            redirect_to categories_path
+        end
+
+    end
+    
+    def require_login
+        if !logged_in?
+            flash[:danger] = "You must be logged in"
+            redirect_to login_path
+
+        end
+
     end
 end
